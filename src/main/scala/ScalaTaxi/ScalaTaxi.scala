6c8e4ef6cd5c6f67f488fb3379b760de6a9d79a5
k.beliaev <Tlt-b@ya.ru>
2017-04-03 16:37:51 +0400

    1

--- src/main/scala/ScalaTaxi/ScalaTaxi.scala
+++ src/main/scala/ScalaTaxi/ScalaTaxi.scala
@@ -1,12 +1,26 @@
 package ScalaTaxi
 
 
-import akka.actor.{Actor, ActorLogging, ActorSystem}
+import akka.actor.{Props, Actor, ActorLogging, ActorSystem}
+import scala.concurrent.duration._
 
 /**
  * Created by k.beliaev on 31.03.2017.
  */
-case class Order (startPoint: String, endPoint: String, needBabySeat: Boolean, needSmoking: Boolean)
+
+case object FreeCar
+case object ReservedCar
+
+case class Order (startPoint: String, endPoint: String, needBabySeat: Boolean, needSmoking: Boolean){
+  val needBabySeatOpt = Option{
+    if (needBabySeat == None) needBabySeat = false
+    else needBabySeat
+  }
+  val needSmokingOpt = Option{
+    if (needSmoking == None) needSmoking = false
+    else needSmoking
+  }
+}
 
 object ScalaTaxi extends App with ActorLogging {
 
@@ -14,6 +28,17 @@ object ScalaTaxi extends App with ActorLogging {
 
   def receive = {
     case Order => log.info(s"received ${loc} from ${sender.path.name}")
+  }
+
+  while (true) {
+    val con = Console.readLine()
+    val conS = con.split("|")
+    conS.length match {
+      case 2 =>
+      case 4 =>
+      case other =>
+        println("Неверный формат заявки.")
+    }
   }
 
 }
@@ -25,6 +50,12 @@ class Car extends Actor {
   val babySeat: Boolean
   val smoking: Boolean
   var carStatus: String
+
+  val taxi = context.actorOf(Props(classOf[Car], self, 0 milliseconds, 50 milliseconds), s"scheduler-for-${self.path.name}")
+
+  override def preStart() = {
+    case FreeCar =>
+  }
 
   def receive = {
 
